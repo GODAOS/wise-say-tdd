180decc895f6cee938af0ae0ebc16465bcc3b93c
GODAOS <jmskkr@gmail.com>
2025-01-02 15:43:59 +0900

    test2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,26 +1,45 @@
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
-        // System.in --> 표준 입력
-
-        // 1. 입력을 봇에게 맡기 수 있다.
-        Scanner scan = new Scanner("등록\n현재를 사랑하라.\n작자미상\n");
-
-        String val1 = scan.nextLine();
-        System.out.println(val1);
-
-        String val2 = scan.nextLine();
-        System.out.println(val2);
-
-        String val3 = scan.nextLine();
-        System.out.println(val3);
-
-        //테스트봇 만들기
-
-        TestApp app = new TestApp();
-        app.run();
-
+        test2();
         }
+
+        public static void test1() {
+            // System.in --> 표준 입력
+
+            // 1. 입력을 봇에게 맡기 수 있다.
+            Scanner scan = new Scanner("등록\n현재를 사랑하라.\n작자미상\n");
+
+            String val1 = scan.nextLine();
+            System.out.println(val1);
+
+            String val2 = scan.nextLine();
+            System.out.println(val2);
+
+            String val3 = scan.nextLine();
+            System.out.println(val3);
+
+            //테스트봇 만들기
+
+            TestApp app = new TestApp();
+            app.run();
+        }
+
+        public static void test2() {
+            // System.out --> 표준 출력 -> 모니터(콘솔)
+
+            // 2. 출력문의 값을 문자열로 받아서 봇에게 줄 수 있다.
+            PrintStream origin = System.out;
+
+            ByteArrayOutputStream out = new ByteArrayOutputStream();// 비어있는 스트림
+            System.setOut(new PrintStream(out));
+            System.out.println("hello");
+
+            String str = out.toString();
+            System.out.println(str);
+        }
+
     }
